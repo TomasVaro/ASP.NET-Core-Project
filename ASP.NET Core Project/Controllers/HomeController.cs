85f405cfbcafc1940fd94bcb3c8083e5d2d04894
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2021-11-10 17:49:46 +0100

    Add project files.

---
+++ ASP.NET Core Project/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Welcome = "Welcome to my homepage!";
+            return View();
+        }
+        public IActionResult About()
+        {
+            //Containing information about yourself (CV, for example)
+            ViewBag.About = "My Resume in Swedish";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            //Containing your contact information (Use fake info if you want).
+            ViewBag.Contact = "My contact information";
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            //Containing the GitHub links to your assignments you have finished with a small description about them.
+            ViewBag.Projects = "My Assignments";
+            return View();
+        }
+    }
+}
