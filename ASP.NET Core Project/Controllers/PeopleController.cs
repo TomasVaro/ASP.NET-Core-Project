6e3b3604a84a85290affe82de489294a88902290
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2021-11-16 13:47:42 +0100

    Added Controller for People, View for ListOfPeople and Models for Person, CreatePersonView and PeopleView.

---
+++ ASP.NET Core Project/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Project.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListOfPeople()
+        {
+            return View();
+        }
+    }
+}
