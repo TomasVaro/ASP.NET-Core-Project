587e67837f4fb2edc94988442ff199845123e8a1
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-02-08 14:35:06 +0100

    Listing people

--- ASP.NET Core Project/Controllers/ReactController.cs
+++ ASP.NET Core Project/Controllers/ReactController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_Core_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,38 @@ namespace ASP.NET_Core_Project.Controllers
 {
     public class ReactController : Controller
     {
+        private readonly AppDbContext _context;
+        public ReactController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public string GetPersons()
+        {
+            List<PersonEFModel> ListOfPersons = _context.People.ToList();
+            List<string> names = _context.People.Select(p => p.Name).ToList();
+
+            //List<CityModel> ListOfCities = _context.City.ToList();
+            //List<LanguageModel> ListOfLanguages = _context.Language.ToList();
+            //List<PersonLanguageModel> ListOfPersonLanguages = _context.PersonLanguage.ToList();
+            //List<CountryModel> Countries = _context.Country.ToList();
+
+            //string JSONresult = JsonConvert.SerializeObject(ListOfPersons, Formatting.Indented,
+            //    new JsonSerializerSettings
+            //    {
+            //        PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            //    });
+
+
+            string JSONresult = JsonConvert.SerializeObject(names);
+            //Response.Write(JSONresult);
+
+
+            //string result = JsonConvert.SerializeObject(DatatableToDictionary(ListOfPersons, "Title"), Newtonsoft.Json.Formatting.Indented);
+            return (JSONresult);
         }
     }
 }
--- ASP.NET Core Project/wwwroot/js/PersonReact.jsx
+++ ASP.NET Core Project/wwwroot/js/PersonReact.jsx
@@ -1,47 +1,151 @@
-﻿//class PersonBox extends React.Component {
-//	render() {
-//		return (
-//			<div className="personBox">Hello, world! I am a person box!</div>
-//		);
-//	}
-//}
-
-//ReactDOM.render(<PersonBox />, document.getElementById('content'));
-
-
-function GetAllPersonsss() {
+﻿function WriteHead() {
 	return (
-		$.get("/Ajax/GetPersons", null, function (data) {
-			//$("#Content").html(data);
-		})
-		//document.getElementById('errorMessages').innerHTML = "";
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Phone</th>
+					<th>City</th>
+					<th>Country</th>
+					<th>Languages</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+		</table>
 		);
 }
+ReactDOM.render(<WriteHead />, document.getElementById('head'));
 
-//ReactDOM.render(<GetAllPersons />, document.getElementById('content'));
 
-axios.get('/Ajax/GetPersons')
-	.then(function (response) {
-		// handle success
-		console.log(response);
-	})
-	.catch(function (error) {
-		// handle error
-		console.log(error);
-	})
-	.finally(function () {
-		// always executed
-	});
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            error: null,
+            persons: []
+        };
+    }
 
-class GetAllPersons extends React.Component {
-	render() {
-		return (
-			<ul>
-				<li>Apples</li>
-				<li>Bananas</li>
-				<li>Cherries</li>
-			</ul>
-			);
+    componentDidMount() {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    // request succesful
+                    var response = xhr.responseText,
+                        json = JSON.parse(response);
+
+                    this.setState({
+                        isLoaded: true,
+                        persons: json
+                    });
+                } else {
+                    // error
+                    this.setState({
+                        isLoaded: true,
+                        error: xhr.responseText
+                    });
+                }
+            }
+        });
+
+        xhr.open("GET", "/React/GetPersons", true);
+        xhr.send();
+    }
+
+    render() {
+        var body;
+        if (!this.state.isLoaded) {
+            // yet loading
+            body = <div>Loading...</div>;
+        } else if (this.state.error) {
+            // error
+            body = <div>Error occured: {this.state.error}</div>
+        } else {
+            // success
+            var persons = this.state.persons;
+            var body = [];
+            for (const element of persons) {
+                body.push(<li>{element}</li>)
+            }
+        }
+        return body;
     }
 }
-ReactDOM.render(<GetAllPersons/>, document.getElementById('content'));
+
+ReactDOM.render(
+    <MyComponent  />,
+    document.getElementById('content')
+);
+
+
+
+
+
+
+
+
+
+////class PersonBox extends React.Component {
+////	render() {
+////		return (
+////			<div className="personBox">Hello, world! I am a person box!</div>
+////		);
+////	}
+////}
+
+////ReactDOM.render(
+////	<PersonBox />, document.getElementById('content')
+////);
+
+
+//function GetAllPersonsss() {
+//	return (
+//		$.get("/Ajax/GetPersons", null, function (data) {
+//			$("#Content").html(data);
+//		})
+//		    document.getElementById('errorMessages').innerHTML = "";
+//		);
+//}
+
+//ReactDOM.render(
+//	<GetAllPersons />, document.getElementById('content')
+//);
+
+//axios.get('/Ajax/GetPersons')
+//	.then(function (response) {
+//		// handle success
+//		console.log(response);
+//	})
+//	.catch(function (error) {
+//		// handle error
+//		console.log(error);
+//	})
+//	.finally(function () {
+//		// always executed
+//	});
+
+//class GetAllPersons extends React.Component {
+//	render() {
+//		return (
+//			<ul>
+//				<li>Apples</li>
+//				<li>Bananas</li>
+//				<li>Cherries</li>
+//			</ul>
+//			);
+//    }
+//}
+//ReactDOM.render(<GetAllPersons />, document.getElementById('content'));
+
+//function GetAllPersons() {
+//	return (
+//		<ul>
+//			<li>Apples</li>
+//			<li>Bananas</li>
+//			<li>Cherries</li>
+//		</ul>
+//	);
+//}
+//ReactDOM.render(<GetAllPersons />, document.getElementById('content'));
